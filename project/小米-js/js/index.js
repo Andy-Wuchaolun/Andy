0df3864d15bda1e89a638c357950a09be3b72088
安迪 <1994200748@qq.com>
2019-09-03 21:01:21 +0800

    js-mi

---
+++ project/小米-js/js/index.js
@@ -0,0 +1,8 @@
+
+var oCart =document.querySelector('.top .cart')
+var oCartBox =document.querySelector('.top .cart-box')
+var oCartContent = document.querySelector('.top .cart-content')
+oCart.onmouseenter = function(){
+	// console.log('aaa')
+	oCartContent.style.height = '100px';
+}
